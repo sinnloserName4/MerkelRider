ebda65dbbd66a1858f644ac639e9d4bd5a5f48c0
ubertroll <lennarthoffmann@gmx.de>
2014-11-21 20:19:08 +0100

    wtf

--- Chopper.js
+++ Chopper.js
@@ -1,15 +1,17 @@
 ﻿function Chopper(_x,_y) {
-    this.leftWheel = game.add.sprite(_x-50, _y+100, "rad");
-    this.rightWheel = game.add.sprite(_x + 50, _y + 100, "rad");
-    this.bike = game.add.sprite(_x, _y, "chopper");
+    this.leftWheel = game.add.sprite(_x-50, _y+120, "rad");
+    this.rightWheel = game.add.sprite(_x + 50, _y + 120, "rad");
+    this.bike = game.add.sprite(_x, _y-30, "chopper");
 
     game.physics.enable([this.leftWheel, this.rightWheel, this.bike], Phaser.Physics.P2JS);
 
     this.leftWheel.body.setCircle(25);
     this.leftWheel.body.debug = true;
+    this.leftWheel.body.mass = 1;
 
     this.rightWheel.body.setCircle(25);
     this.rightWheel.body.debug = true;
+    this.rightWheel.body.mass = 1;
 
     this.bike.body.setRectangle(130, 130);
     this.bike.body.debug = true;
@@ -19,7 +21,7 @@
     this.constraint1 = game.physics.p2.createPrismaticConstraint(this.leftWheel, this.bike, false, [30, 0], [0, 0], [0, 1]);
 
     //SET LIMITS
-    this.constraint1.lowerLimitEnabled = constraint.upperLimitEnabled = true;
+    this.constraint1.lowerLimitEnabled = this.constraint1.upperLimitEnabled = true;
     this.constraint1.upperLimit = -1;
     this.constraint1.lowerLimit = -8;
 
@@ -27,7 +29,7 @@
     this.constraint2 = game.physics.p2.createPrismaticConstraint(this.rightWheel, this.bike, false, [-30, 0], [0, 0], [0, 1]);
 
     //SET LIMITS
-    this.constraint2.lowerLimitEnabled = constraint.upperLimitEnabled = true;
+    this.constraint2.lowerLimitEnabled = this.constraint2.upperLimitEnabled = true;
     this.constraint2.upperLimit = -1;
     this.constraint2.lowerLimit = -8;
 }
--- Merkel.js
+++ Merkel.js
@@ -4,15 +4,15 @@
 function Merkel(_x, _y)
 {
     this.health = 100;
-    this.sprite = game.add.sprite(_x, _y, "merkel");
-    game.physics.enable(this.sprite, Phaser.Physics.P2JS);
+    //this.sprite = game.add.sprite(_x, _y, "merkel");
+    //game.physics.enable(this.sprite, Phaser.Physics.P2JS);
     this.chopper = new Chopper(_x,_y);
 }
 
 Merkel.prototype.handleInput = function ()
 {
     if (arrowkeys.down.isDown) {
-        this.slow();
+        this.Slow();
     }
     if (arrowkeys.up.isDown) {
         this.Accelerate();
@@ -23,10 +23,10 @@ Merkel.prototype.handleInput = function ()
 }
 Merkel.prototype.Slow = function ()
 {
-    this.sprite.body.velocity.x -= SPEED;
+    this.chopper.rightWheel.body.velocity.x -= SPEED;
 }
 
 Merkel.prototype.Accelerate = function ()
 {
-    this.sprite.body.velocity.x += SPEED;
+    this.chopper.rightWheel.body.velocity.x += SPEED;
 }
