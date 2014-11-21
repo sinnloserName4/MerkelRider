5417ca743ea3935e1bc1f08184e4ec14e547936b
ubertroll <lennarthoffmann@gmx.de>
2014-11-21 22:11:22 +0100

    main menu shiiiiiett

--- MainMenu.js
+++ MainMenu.js
@@ -2,11 +2,13 @@
     this.startButton = game.add.button(300, 250, "start_button", startGame, this);
 }
 
+MainMenu.prototype.update = function () {
+
+}
+
+
+
 function startGame() {
     this.startButton.destroy();
     currentState = new Level1();
-}
-
-MainMenu.prototype.update = function () {
-
 }
