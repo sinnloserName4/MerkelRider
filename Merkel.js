06c4c78f6c2eb2968bf3e41c4c05d98e5795f6cf
ubertroll <lennarthoffmann@gmx.de>
2014-11-21 18:54:20 +0100

    8

---
+++ Merkel.js
@@ -0,0 +1,11 @@
+﻿function Merkel() {
+    this.sprite = game.add.sprite(300, 300, "merkel");
+    game.physics.enable(this.sprite, Phaser.Physics.P2JS);
+}
+
+Merkel.prototype.handleInput = function () {
+    if (arrowkeys.left.isDown) {
+
+    }
+}
+
--- game.js
+++ game.js
@@ -1,9 +1,16 @@
-﻿﻿var game = Phaser.game(800, 600, Phaser.AUTO, 'Merkel Rider', { preload: preload, create: create, update: update })
+﻿var game = new Phaser.Game(800, 600, Phaser.AUTO, "", { preload: preload, create: create, update: update });
+
+var arrowkeys;
 
 function preload() {
+    game.load.image("merkel", "content/merkel.jpg");
 }
 
 function create() {
+    game.physics.startSystem(Phaser.Physics.P2JS);
+    game.physics.p2.gravity.y = 300;
+    arrowkeys = game.input.keyboard.createCursorKeys();
+    var m = new Merkel();
 }
 
 function update() {
