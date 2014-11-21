13b3a5cde2211ddac679893d1385f5589bce5d94
ubertroll <lennarthoffmann@gmx.de>
2014-11-21 21:01:39 +0100

    speed yo

--- Merkel.js
+++ Merkel.js
@@ -1,11 +1,12 @@
-﻿var SPEED = 30;
+﻿var forwardSPEED = 15;
+var backwardSPEED = -5;
+var maxSPEED = 500;
+var minSPEED = -300;
 
 
 function Merkel(_x, _y)
 {
     this.health = 100;
-    //this.sprite = game.add.sprite(_x, _y, "merkel");
-    //game.physics.enable(this.sprite, Phaser.Physics.P2JS);
     this.chopper = new Chopper(_x,_y);
 }
 
@@ -38,10 +39,12 @@ Merkel.prototype.RotateLeft = function ()
 
 Merkel.prototype.Slow = function ()
 {
-    this.chopper.rightWheel.body.velocity.x -= SPEED;
+    this.chopper.rightWheel.body.velocity.x = Math.max(this.chopper.rightWheel.body.velocity.x + backwardSPEED, minSPEED);
+    this.chopper.leftWheel.body.velocity.x = Math.max(this.chopper.leftWheel.body.velocity.x + backwardSPEED, minSPEED);
 }
 
 Merkel.prototype.Accelerate = function ()
 {
-    this.chopper.rightWheel.body.velocity.x += SPEED;
+    this.chopper.rightWheel.body.velocity.x = Math.min(this.chopper.rightWheel.body.velocity.x + forwardSPEED, maxSPEED);
+    this.chopper.leftWheel.body.velocity.x = Math.min(this.chopper.leftWheel.body.velocity.x + forwardSPEED, maxSPEED);
 }
