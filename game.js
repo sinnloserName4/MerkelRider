13af5d175ae80eb02b127570b42c2490a0897f7c
sinnloserName4 <nils.poecking@hotmail.de>
2014-11-21 17:35:45 +0100

    lelelele

---
+++ game.js
@@ -0,0 +1,10 @@
+﻿var game = Phaser.game(800, 600, Phaser.AUTO, 'Merkel Rider', { preload: preload, create: create, update: update })
+
+function preload() {
+}
+
+function create() {
+}
+
+function update() {
+}
