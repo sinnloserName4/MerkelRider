e65def7aec5e56d9fcfee4d6651f05ff7181123b
sinnloserName4 <nils.poecking@hotmail.de>
2014-11-22 14:20:57 +0100

    rad

--- game.js
+++ game.js
@@ -12,9 +12,9 @@ function preload()
 {
     game.load.image("merkel", "content/merkel.jpg");
     game.load.image("chopper", "content/chopper.png");
-    game.load.image("rad", "content/rad.png");
+    game.load.image("tire", "content/tire.png");
     game.load.image("start_button", "content/start_game.png");
-    game.load.image("poller","content/poller.png")
+    game.load.image("poller", "content/poller.png");
 }
 
 function create()
